c82edc20ab7f0686b637525cb14029b9c4ac15c9
Andres <andresemm96@gmail.com>
2020-08-20 17:52:34 -0500

    Updates

---
+++ precache-manifest.744b1f3d9e06787ad8929e1ae77f67e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2214812e2376662579e90088657361",
+    "url": "/React-app-Map/index.html"
+  },
+  {
+    "revision": "f74bfd215c36e0a08b08",
+    "url": "/React-app-Map/static/css/2.5b7efe6d.chunk.css"
+  },
+  {
+    "revision": "a3a622b651c40e34d5d8",
+    "url": "/React-app-Map/static/css/main.9fdd35b0.chunk.css"
+  },
+  {
+    "revision": "f74bfd215c36e0a08b08",
+    "url": "/React-app-Map/static/js/2.886ace50.chunk.js"
+  },
+  {
+    "revision": "6701debf1b01cbcdfb21df9957140944",
+    "url": "/React-app-Map/static/js/2.886ace50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3a622b651c40e34d5d8",
+    "url": "/React-app-Map/static/js/main.358ecdbf.chunk.js"
+  },
+  {
+    "revision": "d68447514c79656c16f1",
+    "url": "/React-app-Map/static/js/runtime-main.d7ad92a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-app-Map/static/media/logo.5d5d9eef.svg"
+  }
+]);
